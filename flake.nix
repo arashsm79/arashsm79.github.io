66e2a40f1b04337a2eee1f91258cb70da67aad0f
Arash Sal Moslehian <arashsm79@yahoo.com>
2021-11-15 22:34:33 +0330

    first commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Arash's github.io";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem ["x86_64-linux"]
+      (system:
+        let
+          pkgs = import nixpkgs { system = system; };
+          gems = pkgs.bundlerEnv {
+            name = "arashsm79.github.io-bundlerEnv";
+            ruby = pkgs.ruby;
+            gemdir = ./.;
+          };
+        in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              gems
+              gems.wrappedRuby
+            ];
+          };
+        }
+      );
+}
